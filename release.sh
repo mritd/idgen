4d6d5258725f5502f3e1eafd143b6bb34f8f0c01
mritd <mritd1234@gmail.com>
2018-03-04 12:48:37 +0800

    :bug:fix(0.0.3): 增加 release 版本判断
    增加 release 版本判断 操作失误... 误删除了
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- release.sh
+++ release.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ghr -u mritd -t $GITHUB_RELEASE_TOKEN -replace -recreate --prerelease --debug $1 dist/
+if [ ! -n "$1" ]; then
+    echo "Error:release version is blank!"
+else
+    ghr -u mritd -t $GITHUB_RELEASE_TOKEN -replace -recreate --prerelease --debug $1 dist/
+fi
