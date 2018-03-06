8e208d0286675b8ae9c296a72918d79e2773f7d4
mritd <mritd1234@gimail.com>
2018-03-06 15:06:46 +0900

    :sparkles: feat(0.0.3): release
    release
    
    Signed-off-by: mritd <mritd1234@gimail.com>

--- release.sh
+++ release.sh
@@ -5,5 +5,5 @@ if [ ! -n "$1" ]; then
 else
     git pull
     xgo -targets="darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64" -dest dist -go latest github.com/mritd/idgen
-    ghr -u mritd -t $GITHUB_RELEASE_TOKEN -replace -recreate --prerelease --debug $1 dist/
+    ghr -u mritd -t $GITHUB_RELEASE_TOKEN -replace -recreate --debug $1 dist/
 fi
