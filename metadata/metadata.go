6ea374ccb83a3c70de4a8db59545d129aa4a8900
mritd <mritd1234@gmail.com>
2018-03-06 13:57:35 +0800

    :sparkles: feat(0.0.3): 增加 html 页面自动复制
    增加 html 页面自动复制
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -27,18 +27,42 @@ const VERSION = "v0.0.3"
 const DB_DOWNLOAD_URL = "https://raw.githubusercontent.com/mritd/idgen/master/resources/data.db"
 
 const HtmlTpl = `<!DOCTYPE html>
-<html lang="zh">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>idgen</title>
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" draggable="false"/>
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="keywords" content=""/>
+    <meta name="description" content=""/>
+    <style>
+        .list-li{
+            display: block;
+            width: 100%;
+            border-width: 0px;
+            outline: none;
+            background: #fff;
+        }
+        .list-li::selection{
+            background: rgb(30, 202, 194);
+        }
+    </style>
+  
 </head>
 <body>
-{{ .Name }}<br/>
-{{ .Mobile }}<br/>
-{{ .IdNo }}<br/>
-{{ .Bank }}<br/>
-{{ .Email }}<br/>
-{{ .Addr }}<br/>
+    <input class="list-li" value="{{ .Name }}" />
+    <input class="list-li" value="{{ .Mobile }}" />
+    <input class="list-li" value="{{ .IdNo }}" />
+    <input class="list-li" value="{{ .Bank }}" />
+    <input class="list-li" value="{{ .Email }}" />
+    <input class="list-li" value="{{ .Addr }}" />
+    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        $(".list-li").on('select', function(){
+            document.execCommand('copy', true);
+        });
+    </script>
 </body>
 </html>`
 
