28971fc63d7954b1672acb92e75261bb04da3d19
mritd <mritd@linux.com>
2019-05-12 15:55:38 +0800

    feat(address): rename func
    rename func
    
    Signed-off-by: mritd <mritd@linux.com>

--- generator/address.go
+++ generator/address.go
@@ -8,13 +8,13 @@ import (
 )
 
 // 随机省/城市
-func GenProvinceAndCity() string {
+func GetProvinceAndCity() string {
 	return metadata.ProvinceCity[utils.RandInt(0, len(metadata.ProvinceCity))]
 }
 
 // 随机地址生成
 func GetAddress() string {
-	return GenProvinceAndCity() +
+	return GetProvinceAndCity() +
 		utils.GenRandomLengthChineseChars(2, 3) + "路" +
 		strconv.Itoa(utils.RandInt(1, 8000)) + "号" +
 		utils.GenRandomLengthChineseChars(2, 3) + "小区" +
