e36602877b154c4595dd4a6589647dfe25c8cc08
mritd <mritd@linux.com>
2019-05-12 18:55:40 +0800

    chore(sh): remove release sh
    remove release sh
    
    Signed-off-by: mritd <mritd@linux.com>

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -n "$1" ]; then
-    echo "Error:release version is blank!"
-else
-    git pull
-    xgo -targets="darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64" -dest dist -go latest github.com/mritd/idgen
-    ghr -u mritd -t $GITHUB_RELEASE_TOKEN -replace -recreate --debug $1 dist/
-fi
